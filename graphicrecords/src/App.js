21359178a34e9d7eda2645edfa3c8f7ef6c873ec
Nathan <nathan.forman@gmail.com>
2024-07-24 16:46:54 -0700

    Preparing home page3

--- graphicrecords/src/App.js
+++ graphicrecords/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import record from './img/record.jpg';
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
       <h1>Graphic Records</h1>
       <img src={record}/>
+      {/* <StyledImg src={record}/> */}
     </div>
   );
-}
+};
+
+const StyledImg = styled.img`
+  height: 10rem;
+  width: 10rem;
+`;
 
 export default App;
