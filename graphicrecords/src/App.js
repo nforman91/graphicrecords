025ff40e7fdf3d6ff2fa1d8b7e45f2ee9fb2e493
Nathan <nathan.forman@gmail.com>
2024-07-24 16:13:55 -0700

    Preparing home page2

--- graphicrecords/src/App.js
+++ graphicrecords/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import record from './img/record.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Graphic Records</h1>
+      <img src={record}/>
     </div>
   );
 }
