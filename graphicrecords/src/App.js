c414a3633f1a88da7c544ffec143eefbb9c98327
Nathan <nathan.forman@gmail.com>
2024-07-24 17:21:22 -0700

    Preparing home page5

--- graphicrecords/src/App.js
+++ graphicrecords/src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 import record from './img/record.jpg';
 import styled from "styled-components";
+import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
     <div className="App">
-      <h1>Graphic Records</h1>
+      {/* <GlobalStyle/> */}
+      <StyledTitle>Graphic Records</StyledTitle>
       <img src={record}/>
       {/* <StyledImg src={record}/> */}
     </div>
   );
 };
+
+const StyledTitle = styled.h1`
+  padding-left: 5rem;
+`;
 
 const StyledImg = styled.img`
   height: 10rem;
